7a6a0c396444c44e3d9264e12bdfbacc56eb5301
Kansan1 <2641344238@qq.com>
2023-07-24 20:21:10 +0800

    gt-在线咨询

--- back-end/healthCareSys/healthCareSys-framework/src/main/java/com/acm/config/WebSocketConfiguration.java
+++ back-end/healthCareSys/healthCareSys-framework/src/main/java/com/acm/config/WebSocketConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfiguration {
-
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
