fcae0712792936fceab83d2cebfd1a8394ef8030
Kansan1 <2641344238@qq.com>
2023-07-27 11:17:13 +0800

    根据用途查询药品

---
+++ back-end/healthCareSys/healthCareSys-patient/src/main/java/com/acm/controller/DrugController.java
@@ -0,0 +1,20 @@
+package com.acm.controller;
+
+import com.acm.service.DrugService;
+import com.acm.vo.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DrugController {
+    @Autowired
+    private DrugService drugService;
+
+    @RequestMapping("/getUsageDrugs")
+    public ResponseResult getUsageDrugs(String drugUsage){
+        ResponseResult result =drugService.getUsageDrugs(drugUsage);
+        return result;
+    }
+
+}
