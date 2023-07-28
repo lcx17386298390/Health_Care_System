16a09ec6d99aba46287aa431b9cce37def13ada8
demure <649818851@qq.com>
2023-07-28 14:06:59 +0800

    医生预约订单表格

--- start-end/src/views/doctors/order/index.vue
+++ start-end/src/views/doctors/order/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid px-4">
+  <div class="container-fluid px-4 aaa">
     <h1 class="mt-2">订单处理</h1>
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
@@ -7,31 +7,36 @@
       </li>
       <li class="breadcrumb-item active">表格</li>
     </ol>
-    <div class="card mb-4">
+    <div class="card mb-4 aa">
       <div class="card-header">预约订单列表</div>
       <div class="card-body">
         <div class="datatable-wrapper d-flex flex-column">
-          <div class="datatable-top">
+          <div class="datatable-top d-flex justify-content-between">
             <div class="d-flex justify-content-between">
               <div class="datatable-dropdown">
                 <label>
-                  <select class="datatable-selector">
+                  <select
+                    class="datatable-selector"
+                    v-model="perPage"
+                    @change="handleRowsPerPageChange"
+                  >
                     <option value="5">5</option>
-                    <option value="10" selected="">10</option>
-                    <option value="15">15</option>
+                    <option value="10">10</option>
                   </select>
                   折叠条数显示
                 </label>
               </div>
-              <div class="datatable-search">
-                <input
-                  class="datatable-input"
-                  placeholder="Search..."
-                  type="search"
-                  title="Search within table"
-                  aria-controls="datatablesSimple"
-                />
-              </div>
+            </div>
+            <div class="datatable-search">
+              <input
+                class="datatable-input"
+                v-model="searchQuery"
+                @input="handleSearch"
+                placeholder="Search..."
+                type="search"
+                title="Search within table"
+                aria-controls="datatablesSimple"
+              />
             </div>
           </div>
           <div class="datatable-container">
@@ -49,66 +54,74 @@
               </thead>
               <tbody>
                 <tr v-for="(order, index) in paginatedOrders" :key="index">
-                  <td>{{ order.appointmentDate }}</td>
+                  <td>{{ formattedDate(order.appointmentDate) }}</td>
                   <td>{{ order.patientName }}</td>
                   <td>{{ order.appointmentDepartment }}</td>
-                  <td>{{ order.appointmentStatus }}</td>
+                  <td>
+                    <el-popover
+                      trigger="click"
+                      placement="top-start"
+                      width="200"
+                    >
+                      <template slot="reference">
+                        <el-button
+                          :type="
+                            order.appointmentStatus === '未通过'
+                              ? 'danger'
+                              : 'success'
+                          "
+                        >
+                          {{ order.appointmentStatus }}
+                        </el-button>
+                      </template>
+                      <div>
+                        <p>确认更改预约状态为：</p>
+                        <el-button
+                          type="success"
+                          size="mini"
+                          @click="confirmStatus(order, '通过')"
+                          >通过</el-button
+                        >
+                        <el-button
+                          type="danger"
+                          size="mini"
+                          @click="confirmStatus(order, '未通过')"
+                          >未通过</el-button
+                        >
+                      </div>
+                    </el-popover>
+                  </td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
-        <Pagination
-          v-if="orders.length > perPage"
-          :perPage="perPage"
-          :total="orders.length"
-          @input="setCurrentPage"
-        />
         <div class="datatable-bottom">
-          <div class="datatable-info">Showing 1 to 10 of 57 entries</div>
+          <div class="datatable-info">
+            Showing {{ startIndex }} to {{ endIndex }} of
+            {{ totalEntries }} entries
+          </div>
           <nav class="datatable-pagination">
             <ul class="datatable-pagination-list">
               <li
-                class="datatable-pagination-list-item datatable-hidden datatable-disabled"
+                v-if="currentPage > 1"
+                @click="setCurrentPage(currentPage - 1)"
               >
-                <a data-page="1" class="datatable-pagination-list-item-link"
-                  >‹</a
-                >
+                <a class="datatable-pagination-list-item-link">‹</a>
               </li>
-              <li class="datatable-pagination-list-item datatable-active">
-                <a data-page="1" class="datatable-pagination-list-item-link"
-                  >1</a
-                >
+              <li
+                v-for="page in totalPages"
+                :key="page"
+                :class="{ 'datatable-active': currentPage === page }"
+                @click="setCurrentPage(page)"
+              >
+                <a class="datatable-pagination-list-item-link">{{ page }}</a>
               </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="2" class="datatable-pagination-list-item-link"
-                  >2</a
-                >
-              </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="3" class="datatable-pagination-list-item-link"
-                  >3</a
-                >
-              </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="4" class="datatable-pagination-list-item-link"
-                  >4</a
-                >
-              </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="5" class="datatable-pagination-list-item-link"
-                  >5</a
-                >
-              </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="6" class="datatable-pagination-list-item-link"
-                  >6</a
-                >
-              </li>
-              <li class="datatable-pagination-list-item">
-                <a data-page="2" class="datatable-pagination-list-item-link"
-                  >›</a
-                >
+              <li
+                v-if="currentPage < totalPages"
+                @click="setCurrentPage(currentPage + 1)"
+              >
+                <a class="datatable-pagination-list-item-link">›</a>
               </li>
             </ul>
           </nav>
@@ -119,11 +132,8 @@
 </template>
 
 <script>
-import Pagination from 'vue-pagination-2'
 export default {
-  components: {
-    Pagination,
-  },
+  components: {},
   data() {
     return {
       orders: [
@@ -196,71 +206,104 @@ export default {
       ],
       perPage: 10,
       currentPage: 1,
+      searchQuery: "",
     };
   },
   computed: {
     paginatedOrders() {
       const start = (this.currentPage - 1) * this.perPage;
       const end = start + this.perPage;
-      return this.orders.slice(start, end);
+      if (this.searchQuery) {
+        const lowercaseQuery = this.searchQuery.toLowerCase();
+        return this.orders
+          .filter(
+            (order) =>
+              order.patientName.toLowerCase().includes(lowercaseQuery) ||
+              order.appointmentDepartment.toLowerCase().includes(lowercaseQuery)
+          )
+          .slice(start, end);
+      } else {
+        return this.orders.slice(start, end);
+      }
+    },
+    formattedDate() {
+      return (dateString) => {
+        const date = new Date(dateString);
+        return `${date.getFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+      };
+    },
+    totalEntries() {
+      return this.orders.length;
+    },
+    totalPages() {
+      return Math.ceil(this.totalEntries / this.perPage);
+    },
+    startIndex() {
+      return (this.currentPage - 1) * this.perPage + 1;
+    },
+    endIndex() {
+      return Math.min(this.currentPage * this.perPage, this.totalEntries);
     },
   },
   methods: {
     setCurrentPage(page) {
       this.currentPage = page;
     },
+    handleSearch() {
+      this.currentPage = 1;
+    },
+    confirmStatus(order, status) {
+      this.$confirm(`确认更改预约状态为${status}?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          order.appointmentStatus = status;
+          this.$notify({
+            title: "成功",
+            message: "预约状态已更新",
+            type: "success",
+          });
+        })
+        .catch(() => {
+
+        });
+    },
   },
 };
 </script>
 
 <style scoped>
+@media screen and (max-width: 768px) {
+  /* Adjust the table font size for smaller screens */
+  .table-bordered th,
+  .table-bordered td {
+    font-size: 12px;
+  }
+}
+
+.datatable-wrapper {
+  /* Add overflow property to handle content overflow */
+  overflow: auto;
+}
+.aa{
+  max-height: 80%;
+}
+.aaa{
+  height: 100vh;
+}
 .datatable-top > nav:last-child,
 .datatable-top > div:last-child,
 .datatable-bottom > nav:last-child,
 .datatable-bottom > div:last-child {
   float: right;
 }
-.datatable-pagination li.datatable-hidden {
-  visibility: visible;
-}
 .datatable-pagination li {
   list-style: none;
   float: left;
-}
-.page-item:first-child .page-link,
-.page-item:first-child .datatable-pagination a,
-.datatable-pagination .page-item:first-child a,
-.datatable-pagination li:first-child .page-link,
-.datatable-pagination li:first-child a {
-  border-top-left-radius: var(--bs-pagination-border-radius);
-  border-bottom-left-radius: var(--bs-pagination-border-radius);
-}
-.datatable-pagination .datatable-disabled a,
-.datatable-pagination .datatable-disabled a:focus,
-.datatable-pagination .datatable-disabled a:hover {
-  cursor: not-allowed;
-  opacity: 0.4;
-}
-.datatable-pagination .datatable-disabled a,
-.datatable-pagination .datatable-disabled a:focus,
-.datatable-pagination .datatable-disabled a:hover,
-.datatable-pagination .datatable-ellipsis a {
-  pointer-events: none;
-  cursor: default;
-}
-.page-link,
-.datatable-pagination a {
-  position: relative;
-  display: block;
-  padding: var(--bs-pagination-padding-y) var(--bs-pagination-padding-x);
-  font-size: var(--bs-pagination-font-size);
-  color: var(--bs-pagination-color);
-  text-decoration: none;
-  background-color: var(--bs-pagination-bg);
-  border: var(--bs-pagination-border-width) solid
-    var(--bs-pagination-border-color);
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 .datatable-pagination a {
   border: 1px solid transparent;
@@ -271,40 +314,6 @@ export default {
   text-decoration: none;
   color: #333;
   cursor: pointer;
-}
-a {
-  color: var(--bs-link-color);
-  text-decoration: underline;
-}
-.pagination,
-.datatable-pagination ul {
-  --bs-pagination-padding-x: 0.75rem;
-  --bs-pagination-padding-y: 0.375rem;
-  --bs-pagination-font-size: 1rem;
-  --bs-pagination-color: var(--bs-link-color);
-  --bs-pagination-bg: #fff;
-  --bs-pagination-border-width: 1px;
-  --bs-pagination-border-color: #dee2e6;
-  --bs-pagination-border-radius: 0.375rem;
-  --bs-pagination-hover-color: var(--bs-link-hover-color);
-  --bs-pagination-hover-bg: #e9ecef;
-  --bs-pagination-hover-border-color: #dee2e6;
-  --bs-pagination-focus-color: var(--bs-link-hover-color);
-  --bs-pagination-focus-bg: #e9ecef;
-  --bs-pagination-focus-box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
-  --bs-pagination-active-color: #fff;
-  --bs-pagination-active-bg: #0d6efd;
-  --bs-pagination-active-border-color: #0d6efd;
-  --bs-pagination-disabled-color: #6c757d;
-  --bs-pagination-disabled-bg: #fff;
-  --bs-pagination-disabled-border-color: #dee2e6;
-  display: flex;
-  padding-left: 0;
-  list-style: none;
-}
-.datatable-pagination ul {
-  margin: 0;
-  padding-left: 0;
 }
 .datatable-selector {
   padding: 0.375rem 1.75rem 0.375rem 0.75rem;
@@ -318,10 +327,35 @@ a {
   background-size: 8px 10px;
 }
 .datatable-input {
-  padding: 0.375rem 1.75rem 0.375rem 0.75rem;
+  float: right;
+  padding: 0.375rem 1.55rem 0.375rem 0.75rem;
   height: 2.25rem;
   font-size: 0.875rem;
   border-radius: 0.375rem;
   border: 1px solid #ced4da;
+}
+.table-bordered th,
+.table-bordered td {
+  border: 1px solid #dee2e6;
+  padding: 0.55rem;
+  background-color: #f7f9fb;
+  text-align: center;
+}
+.table-bordered tr:hover {
+  cursor: pointer;
+}
+.datatable-info {
+  float: left;
+}
+
+.datatable-pagination {
+  float: right;
+}
+
+.datatable-pagination-list {
+  list-style: none;
+  padding-left: 0;
+  margin: 0;
+  display: flex;
 }
 </style>
