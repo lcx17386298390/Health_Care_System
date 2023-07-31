cba7b53a3c9023c20c19a56a3788ba437010b9a4
Violet <3104044743@qq.com>
2023-07-31 19:31:53 +0800

    bug

--- back-end/healthCareSys/healthCareSys-framework/src/main/java/com/acm/service/impl/DoctorServiceImpl.java
+++ back-end/healthCareSys/healthCareSys-framework/src/main/java/com/acm/service/impl/DoctorServiceImpl.java
@@ -1,20 +1,32 @@
 package com.acm.service.impl;
 
 import com.acm.entity.Doctor;
+import com.acm.entity.LoginDoc;
+import com.acm.entity.LoginUser;
+import com.acm.entity.Patient;
 import com.acm.enums.AppHttpCodeEnum;
+import com.acm.exception.SystemException;
 import com.acm.mapper.DoctorMapper;
 import com.acm.service.DoctorService;
 import com.acm.utils.BeanCopyUtils;
-import com.acm.vo.DoctorVo;
-import com.acm.vo.DoctorsByDepartmentVo;
-import com.acm.vo.ResponseResult;
+import com.acm.utils.JwtUtil;
+import com.acm.utils.RedisCache;
+import com.acm.vo.*;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -29,6 +41,14 @@ public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor> impleme
 
     @Resource
     private DoctorMapper doctorMapper;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     //    根据科室来查询医生
     @Override
@@ -106,5 +126,85 @@ public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor> impleme
         }else{
             return ResponseResult.errorResult(AppHttpCodeEnum.DOCTOR_NOT_EXIST);
         }
+    }
+
+
+    @Override
+    public ResponseResult login(Doctor doctor) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(doctor.getUsername(), doctor.getPassword());
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        if (Objects.isNull(authentication)) {
+            throw new RuntimeException("用户名或密码错误");
+        }
+        LoginDoc loginDoc = (LoginDoc) authentication.getPrincipal();
+        String id = loginDoc.getDoctor().getId().toString();
+        String jwt = JwtUtil.createJWT(id);
+        redisCache.setCacheObject("doctorlogin:" + id, loginDoc);
+        DoctorVo doctorVo = BeanCopyUtils.copyBean(loginDoc.getDoctor(), DoctorVo.class);
+        DoctorLoginVo doctorInfoVo = new DoctorLoginVo(jwt,doctorVo);
+        return ResponseResult.okResult(doctorInfoVo);
+    }
+
+    @Override
+    public ResponseResult logout() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LoginDoc principal = (LoginDoc) authentication.getPrincipal();
+        Long userId = Long.valueOf(principal.getDoctor().getId());
+        redisCache.deleteObject("doctorlogin:" + userId);
+        return ResponseResult.okResult();
+    }
+
+
+    @Override
+    public ResponseResult register(Doctor doctor) {
+        if(!StringUtils.hasText(doctor.getUsername())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if(!StringUtils.hasText(doctor.getPassword())){
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if(!StringUtils.hasText(doctor.getEmail())){
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if(!StringUtils.hasText(doctor.getRealname())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_NOT_NULL);
+        }
+        //对数据进行是否存在的判断
+        if(userNameExist(doctor.getUsername())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+        if(nickNameExist(doctor.getRealname())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        //...
+        //对密码进行加密
+        String encodePassword = passwordEncoder.encode(doctor.getPassword());
+        doctor.setPassword(encodePassword);
+        //存入数据库
+        save(doctor);
+        return ResponseResult.okResult();
+    }
+
+    /**
+     * 检测用户名是否存在
+     * @param userName
+     * @return
+     */
+    private boolean userNameExist(String userName) {
+        LambdaQueryWrapper<Doctor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Doctor::getUsername,userName);
+        return count(queryWrapper) > 0;
+    }
+
+    /**
+     * 是否输入真名
+     * @param nickName
+     * @return
+     */
+    private boolean nickNameExist(String nickName) {
+        LambdaQueryWrapper<Doctor> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Doctor::getRealname,nickName);
+        return count(queryWrapper) > 0;
     }
 }
