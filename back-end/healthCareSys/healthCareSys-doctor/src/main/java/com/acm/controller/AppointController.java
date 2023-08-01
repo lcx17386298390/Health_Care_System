e9ce3dc0ba55f811da2d2e9ebab43733252bdb67
Kansan1 <2641344238@qq.com>
2023-08-01 09:19:00 +0800

    gt-医生功能

---
+++ back-end/healthCareSys/healthCareSys-doctor/src/main/java/com/acm/controller/AppointController.java
@@ -0,0 +1,26 @@
+package com.acm.controller;
+
+
+import com.acm.service.AppointmentService;
+import com.acm.vo.ResponseResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/doctor")
+public class AppointController {
+
+
+    @Resource
+    private AppointmentService appointmentService;
+
+//    查询就诊记录
+    @RequestMapping("/getappointmentBydname")
+    public ResponseResult getappointmentBydname(Integer pageNum,Integer pageSize,String docName){
+        return appointmentService.getappointmentBydname(pageNum,pageSize,docName);
+    }
+
+
+}
